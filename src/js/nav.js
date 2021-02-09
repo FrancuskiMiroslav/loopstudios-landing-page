e5caf87da60ea0be19372709521a50d1b0b74523
Francuski Miroslav <francuski.miroslav@gmail.com>
2021-02-09 14:25:55 +0100

    added sticky nav functionality with hamburger

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,21 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const headerContainer = document.getElementById('header-container');
+	const stickyHeader = document.getElementById('sticky-header');
 	const menuBtn = document.getElementById('menu-btn');
 	const navList = document.getElementById('nav-list');
 
 	window.addEventListener('scroll', (e) => {
 		if (document.documentElement.scrollTop > 0) {
-			headerContainer.classList.add('sticky');
+			stickyHeader.classList.add('sticky');
 			/* btnScrollToTop.style.opacity = 1; */
 		} else {
-			headerContainer.classList.remove('sticky');
+			stickyHeader.classList.remove('sticky');
 			/* 	btnScrollToTop.style.opacity = 0; */
 		}
 	});
 
 	menuBtn.addEventListener('click', (e) => {
 		menuBtn.classList.toggle('clicked');
-		/* 	headerContainer.classList.toggle('overlay');
-		navList.classList.toggle('show'); */
+		headerContainer.classList.toggle('overlay');
+		navList.classList.toggle('show');
 	});
 });
